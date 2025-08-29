260a3de32a7b59379e08e44a13eb2c6336d4f47f
RizkiAlhamid <riskimifta@gmail.com>
2025-08-29 09:50:57 -0700

    idk

--- init.lua
+++ init.lua
@@ -428,7 +428,25 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         -- gopls = {},
-        pyright = {},
+        pyright = {
+          on_init = function(client)
+            client.config.settings = client.config.settings or {}
+            client.config.settings.python = client.config.settings.python or {}
+            client.config.settings.python.pythonPath = (function(workspace)
+              if vim.env.VIRTUAL_ENV then
+                return vim.fs.joinpath(vim.env.VIRTUAL_ENV, 'bin', 'python')
+              end
+              if workspace then
+                local poetry_lock_path = vim.fs.joinpath(workspace, 'poetry.lock')
+                if vim.fn.filereadable(poetry_lock_path) == 1 then
+                  local venv = vim.fn.trim(vim.fn.system 'poetry env info -p')
+                  return vim.fs.joinpath(venv, 'bin', 'python')
+                end
+              end
+              return vim.fn.exepath 'python3' or vim.fn.exepath 'python' or 'python'
+            end)(client.config.root_dir)
+          end,
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
